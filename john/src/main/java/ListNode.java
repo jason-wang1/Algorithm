592506fa7c2b42c33f3fcf5bf62aca01cdfdf102
郑隆 <zhenglong@baijiahulian.com>
2020-12-01 22:30:51 +0800

    ``

---
+++ john/src/main/java/ListNode.java
@@ -0,0 +1,22 @@
+/**
+ * ClassName ListNode
+ * Description
+ * Create by long
+ * CopyRight www.baijiahulian.com
+ * group bigdata-dw
+ * Date 12/1/20 10:28 PM
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+
+    public  ListNode(int val,ListNode next){
+        this.val=val;
+        this.next=next;
+
+    }
+
+
+
+
+}
