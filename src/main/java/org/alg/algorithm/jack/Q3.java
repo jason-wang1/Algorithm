0acb13a57765bb812c3000be3b693ca3b2ccdc6a
wubeibei <wubeibei@zhihu.com>
2020-12-01 19:39:33 +0800

    第三天 2b 题

---
+++ src/main/java/org/alg/algorithm/jack/Q3.java
@@ -0,0 +1,53 @@
+package java.org.alg.algorithm.jack;
+
+import java.util.Stack;
+
+/**
+ *剑指 Offer 09. 用两个栈实现队列
+ * 用两个栈实现一个队列。队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，分别完成在队列尾部插入整数和在队列头部删除整数的功能。
+ * (若队列中没有元素，deleteHead 操作返回 -1 )
+ * 示例 1：
+ * 输入：
+ * ["CQueue","appendTail","deleteHead","deleteHead"]
+ * [[],[3],[],[]]
+ * 输出：[null,null,3,-1]
+ * 示例 2：
+ * 输入：
+ * ["CQueue","deleteHead","appendTail","appendTail","deleteHead","deleteHead"]
+ * [[],[],[5],[2],[],[]]
+ * 输出：[null,-1,null,null,5,2]
+ */
+public class Q3 {
+    class CQueue {
+        Stack<Integer> stack1;
+        Stack<Integer>  stack2;
+        public CQueue() {
+            stack1=new Stack();
+            stack2=new Stack();
+        }
+
+        public void appendTail(int value) {
+            stack1.push(value);
+        }
+
+        public int deleteHead() {
+            if(stack2.isEmpty()){
+                while(!stack1.isEmpty()){
+                    stack2.push(stack1.pop());
+                }
+            }
+            if(stack2.isEmpty()){
+                return -1;
+            }else{
+                return stack2.pop();
+            }
+        }
+    }
+
+/**
+ * Your CQueue object will be instantiated and called as such:
+ * CQueue obj = new CQueue();
+ * obj.appendTail(value);
+ * int param_2 = obj.deleteHead();
+ */
+}
