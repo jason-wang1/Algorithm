959574cecee3ee650c9b3d802c4732b5d819e71f
wangbo <1016765655@qq.com>
2020-12-04 15:46:07 +0800

    improved Fibonacci

--- src/main/java/org/alg/algorithm/jason/recursive/Fibonacci.java
+++ src/main/java/org/alg/algorithm/jason/recursive/Fibonacci.java
@@ -1,5 +1,8 @@
 package org.alg.algorithm.jason.recursive;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Descreption: 递归
  * 求斐波拉契数列第n项
@@ -11,15 +14,36 @@ package org.alg.algorithm.jason.recursive;
  */
 public class Fibonacci {
     public static void main(String args[]){
-        System.out.println(get(7));
+        System.out.println(getFib1(45));
     }
 
-    public static int get(int n){
-        // n = 1,2 时，初始状态
-        if (n == 1 || n == 2)
-            return 1;
+    /**
+     * 使用递归获取斐波拉契数列第n项，这里存在大量重复计算
+     */
+    public static long getFib1(long n){
+        // n = 0,1 时，初始状态
+        if (n < 2)
+            return n;
 
-        // n > 2 时，非初始状态，假设前一状态已经解决
-        return get(n-1) + get(n-2);
+        // n >= 2 时，非初始状态，假设前一状态已经解决
+        return getFib1(n-1) + getFib1(n-2);
+    }
+
+    private static Map<Integer, Integer> cache = new HashMap<Integer, Integer>();
+
+    public static int getFib2(int n){
+        // n = 0,1 时，初始状态
+        if (n < 2)
+            return n;
+
+        // n >= 2 时，非初始状态，假设前一状态已经解决
+        Integer res = cache.get(n);
+        if (res != null){
+            return res;
+        } else {
+            int val = getFib2(n - 1) + getFib2(n - 2);
+            cache.put(n, val);
+            return val;
+        }
     }
 }
