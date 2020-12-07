1c09d2dd16a7230fe278825ee02e86c134865991
wangbo <1016765655@qq.com>
2020-12-07 12:27:56 +0800

    add a new method with DP

--- src/main/java/org/alg/algorithm/jason/recursive/Fibonacci.java
+++ src/main/java/org/alg/algorithm/jason/recursive/Fibonacci.java
@@ -52,4 +52,18 @@ public class Fibonacci {
             return val;
         }
     }
+
+
+    /**
+     * 使用动态规划/迭代计算斐波拉契数列第n项
+     */
+    public static int getFib3(int n){
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
 }
